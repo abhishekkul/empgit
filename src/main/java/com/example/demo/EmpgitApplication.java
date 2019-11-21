e598ac8585552728279d472368e1442a060bf164
lenovo <lenovo@DESKTOP-3EO33O0>
2019-11-21 10:21:41 +0530

    check code

---
+++ src/main/java/com/example/demo/EmpgitApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpgitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpgitApplication.class, args);
+	}
+
+}
